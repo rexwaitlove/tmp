a3eb4f6343cf5c7c87d204ba351a0a694bc0dd98
F.C. Huang <fuchang.huang@gmail.com>
2013-08-15 12:47:33 +0800

    modify index and world ...

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<php
+<?php
 	echo "Hello Test !";
 	echo "Test staging area";
 	echo "Test commit";
 	echo "Test Welcome to bug!!!!";
 
 	echo "Test server Github!!";
-	echo "At facebook  work"
-	echo "At google plus+ work"
+	echo "At facebook  work";
+	echo "At google plus+ work";
+
+	echo "New branch ,test stash!!";
 ?>
